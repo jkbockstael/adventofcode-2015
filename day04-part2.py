350df5524ad8ce392f26fb3ecc7922f792872852
Jean-Karim Bockstael <jkb@jkbockstael.be>
2015-12-04 20:12:01 +0100

    Solutions for Day 4 parts 1 and 2.

---
+++ day04-part2.py
@@ -0,0 +1,16 @@
+# An Advent of Code 2015 - Day 4 - The Ideal Stocking Stuffer - Part 2
+# http://adventofcode.com/day/4
+
+import sys
+import hashlib
+
+# Find the lowest integer suffix that produces a MD5 hash starting with six zeroes
+def find_lowest_suffix(key):
+    suffix = 0
+    while (hashlib.md5(bytes(key + str(suffix), 'utf-8')).hexdigest()[:6] != '000000'):
+        suffix += 1
+    return suffix
+
+# Main
+key = sys.stdin.readline().strip()
+print(find_lowest_suffix(key))
