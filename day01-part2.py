4ecc3dde6eefc6d1b0dc3113043105ae482aa3d3
Jean-Karim Bockstael <jkb@jkbockstael.be>
2015-12-04 02:20:13 +0100

    Solutions for Day 01 parts 1 and 2.

---
+++ day01-part2.py
@@ -0,0 +1,26 @@
+# An Advent of Code 2015 - Day 1 - Not Quite Lisp - Part 2
+# http://adventofcode.com/day/1
+
+import sys
+
+
+# Return the number of instructions to follow before reaching the basement (-1)
+# Santa starts on the ground floor (0)
+# - '(' in the directions makes him go up one floor
+# - ')' in the directions makes him go down floor
+def steps_to_enter_basement(directions):
+    steps = 0
+    floor = 0
+    for step in directions:
+        steps += 1
+        if step == '(':
+            floor += 1
+        if step == ')':
+            floor -= 1
+        if floor == -1:
+            return steps
+    return None
+
+# Main
+directions = sys.stdin.readline().strip()
+print(steps_to_enter_basement(directions))
---
+++ day01.py
@@ -0,0 +1,21 @@
+# An Advent of Code 2015 - Day 1 - Not Quite Lisp
+# http://adventofcode.com/day/1
+
+import sys
+
+# Return the floor reached by following given directions
+# Santa starts on the ground floor (0)
+# - '(' in the directions makes him go up one floor
+# - ')' in the directions makes him go down floor
+def final_floor(directions):
+    floor = 0
+    for step in directions:
+        if step == '(':
+            floor += 1
+        if step == ')':
+            floor -= 1
+    return floor
+
+# Main
+directions = sys.stdin.readline().strip()
+print(final_floor(directions))
