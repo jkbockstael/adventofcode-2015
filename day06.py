1b1ed5b13a209da2178948bc21752487b05bfdff
Jean-Karim Bockstael <jkb@jkbockstael.be>
2015-12-07 00:38:39 +0100

    Solutions for Day 6 parts 1 and 2.

---
+++ day06.py
@@ -0,0 +1,44 @@
+# Advent of Code 2015 - Day 6 - Probably a Fire Hazard
+# http://adventofcode.com/day/6
+
+import sys
+
+# Run one instruction and return the resulting lights array
+def run_instruction(instruction, lights):
+    action, x_min, y_min, x_max, y_max = parse_instruction(instruction)
+    for x in range(x_min, x_max + 1):
+        for y in range(y_min, y_max + 1):
+            if action == 'turn on':
+                lights[x][y] = True
+            elif action == 'turn off':
+                lights[x][y] = False
+            elif action == 'toggle':
+                lights[x][y] = not lights[x][y]
+    return lights
+
+# Parse one instruction
+def parse_instruction(instruction):
+    if instruction.startswith('turn on'):
+        action = 'turn on'
+    elif instruction.startswith('turn off'):
+        action = 'turn off'
+    elif instruction.startswith('toggle'):
+        action = 'toggle'
+    else:
+        action = None
+    corners = instruction[len(action):].strip().split(' through ')
+    x_min, y_min = map(int, corners[0].split(','))
+    x_max, y_max = map(int, corners[1].split(','))
+    return action, x_min, y_min, x_max, y_max
+
+# Count the lights that are in a lit state
+def count_lit_lights(lights):
+    return sum(map(sum, lights))
+
+# Main
+lights = []
+for x in range(1000):
+    lights.append([False] * 1000)
+for instruction in sys.stdin:
+    lights = run_instruction(instruction, lights)
+print(count_lit_lights(lights))
