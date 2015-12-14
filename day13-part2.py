044fd17f23eeaed133b0f2a95cffac9091175e53
Jean-Karim Bockstael <jkb@jkbockstael.be>
2015-12-15 00:03:44 +0100

    Solution for Day 13 part 2.

---
+++ day13-part2.py
@@ -0,0 +1,18 @@
+# Advent of Code 2015 - Day 13 - Knights of the Dinner Table - Part 2
+# http://adventofcode.com/day/13
+
+from day13 import *
+
+# Add me to the guests list, as a neutral element
+def add_me(guests):
+    guest_names = guests.keys()
+    guests['Me'] = {}
+    for guest_name in guest_names:
+        guests['Me'][guest_name] = 0
+        guests[guest_name]['Me'] = 0
+    return guests
+
+# Main
+guests = parse_guest_list(sys.stdin.readlines())
+guests = add_me(guests)
+print(best_seating_outcome(guests))
