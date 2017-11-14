56ddb2f274a6cb09f679c4d47f9a954f47df9df1
Jean-Karim Bockstael <jkb@jkbockstael.be>
2017-11-14 19:28:45 +0100

    Refactored Day 4.

--- day04_part1.py
+++ day04_part1.py
@@ -4,13 +4,14 @@
 import sys
 import hashlib
 
-# Find the lowest integer suffix that produces a MD5 hash starting with five zeroes
-def find_lowest_suffix(key):
+# Find the lowest integer suffix that produces a MD5 hash starting with a given prefix
+def find_lowest_suffix(required_prefix, key):
     suffix = 0
-    while (not hashlib.md5(bytes(key + str(suffix), 'utf-8')).hexdigest().startswith('00000')):
+    while (not hashlib.md5(bytes(key + str(suffix), 'utf-8')).hexdigest().startswith(required_prefix)):
         suffix += 1
     return suffix
 
 # Main
-key = sys.stdin.readline().strip()
-print(find_lowest_suffix(key))
+if __name__ == '__main__':
+    key = sys.stdin.readline().strip()
+    print(find_lowest_suffix('00000', key))
--- day04_part2.py
+++ day04_part2.py
@@ -1,16 +1,8 @@
 # Advent of Code 2015 - Day 4 - The Ideal Stocking Stuffer - Part 2
 # http://adventofcode.com/2015/day/4
 
-import sys
-import hashlib
-
-# Find the lowest integer suffix that produces a MD5 hash starting with six zeroes
-def find_lowest_suffix(key):
-    suffix = 0
-    while (not hashlib.md5(bytes(key + str(suffix), 'utf-8')).hexdigest().startswith('000000')):
-        suffix += 1
-    return suffix
+from day04_part1 import *
 
 # Main
 key = sys.stdin.readline().strip()
-print(find_lowest_suffix(key))
+print(find_lowest_suffix('000000', key))
